c5ccfc90905e707e9f3dcb438b022fa99ed6d7fb
Javier Ordonez <javier.bernaldez@gmail.com>
2024-02-28 18:37:05 +0100

    fix: router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    { path: '', loadComponent: () => import('./list-super-heros/list-super-heros.component').then(com => com.ListSuperHerosComponent) },
-    { path: ':id', loadComponent: () => import('./detail-super-hero/detail-super-hero.component').then(com => com.DetailSuperHeroComponent) }
+    { path: '', loadComponent: () => import('./list-super-heros/list-super-heros.component').then(com => com.ListSuperHerosComponent) }
 ];
